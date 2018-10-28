c0baeab85fe2ac7f763ceedcb5751575ce69af65
Daniel <dbaldesarra9@gmail.com>
2018-10-28 00:26:21 -0400

    teams page animation

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -3,17 +3,18 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import Grow from '@material-ui/core/Grow'
 
 export default class TeamCard extends React.Component {
   render() {
     return (
-      <Card>
-        <CardActionArea>
-          {/* <CardMedia
+      <Grow in={true} timeout={{enter: this.props.delay}}>
+        <Card>
+          <CardActionArea>
+            {/* <CardMedia
             component="img"
             alt="Contemplative Reptile"
             className={classes.media}
@@ -21,18 +22,19 @@ export default class TeamCard extends React.Component {
             image="/static/images/cards/contemplative-reptile.jpg"
             title="Contemplative Reptile"
           /> */}
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {this.props.teamName}
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {this.props.teamName}
               </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button component={NavLink} to={`team/${this.props.teamId}`} size="large" color="primary">
-            See Stats
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button component={NavLink} to={`team/${this.props.teamId}`} size="large" color="primary">
+              See Stats
             </Button>
-        </CardActions>
-      </Card>
+          </CardActions>
+        </Card>
+      </Grow>
     )
   }
 } 
--- src/components/Teams.js
+++ src/components/Teams.js
@@ -7,15 +7,17 @@ import {
 
 export default class Teams extends React.Component {
   render() {
+    let count = 200
     return (
       <div style={{ padding: 20 }}>
         <Grid container spacing={24}>
           {
-            teams.list.map((item, key) =>
-              (<Grid key={key} item xs>
-                <TeamCard teamName={item.name} teamId={item.id} key={key} />
+            teams.list.map((item, key) => {
+              count += 200
+              return (<Grid key={key} item xs>
+                <TeamCard teamName={item.name} teamId={item.id} key={key} delay={count} />
               </Grid>)
-            )
+            })
           }
         </Grid>
       </div>
